d2f45060de2c4ec88a4d06ffd47da1475f26a3d2
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-12-07 15:06:13 +0300

    feat: adding js implementation

---
+++ index.js
@@ -0,0 +1,114 @@
+const readline = require('readline');
+
+const wallet = {
+  balance: 0,
+  transactions: [],
+  history: [],
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function addIncome(amount) {
+  wallet.balance += amount;
+  wallet.transactions.push({ type: 'income', amount });
+  wallet.history.push({ action: 'Added income', amount });
+  console.log(`Added ${amount} to your wallet. New balance: ${wallet.balance}`);
+}
+
+function subtractExpense(amount) {
+  if (amount > wallet.balance) {
+    console.log('Insufficient funds');
+    return;
+  }
+  wallet.balance -= amount;
+  wallet.transactions.push({ type: 'expense', amount });
+  wallet.history.push({ action: 'Subtracted expense', amount });
+  console.log(`Subtracted ${amount} from your wallet. New balance: ${wallet.balance}`);
+}
+
+function viewBalance() {
+  console.log(`Current balance: ${wallet.balance}`);
+  console.table(wallet.transactions);
+}
+
+function viewHistory() {
+  console.log('--- History ---');
+  for (const entry of wallet.history) {
+    console.log(`${entry.action}: ${entry.amount}`);
+  }
+  console.log('');
+}
+
+function flushWallet() {
+  console.log('Are you sure you want to flush your wallet? This will remove all transactions and history.');
+  rl.question('(y/n): ', (answer) => {
+    if (answer === 'y') {
+      wallet.balance = 0;
+      wallet.transactions = [];
+      wallet.history = [];
+      console.log('Wallet flushed successfully.');
+    } else {
+      console.log('Flushing canceled.');
+    }
+    mainMenu();
+  });
+}
+
+function exit() {
+  console.log('Exiting the application.');
+  rl.close();
+}
+
+function mainMenu() {
+  console.log('Welcome to your Wallet Manager!');
+  console.log('1. Add Income');
+  console.log('2. Subtract Expense');
+  console.log('3. View Balance');
+  console.log('4. View History');
+  console.log('5. Flush Wallet');
+  console.log('6. Exit');
+
+  rl.question('Enter your choice: ', (option) => {
+    switch (option) {
+      case '1':
+        rl.question('Enter income amount: ', (income) => {
+          addIncome(parseFloat(income));
+          mainMenu();
+        });
+        break;
+      case '2':
+        rl.question('Enter expense amount: ', (expense) => {
+          subtractExpense(parseFloat(expense));
+          mainMenu();
+        });
+        break;
+      case '3':
+        viewBalance();
+        mainMenu();
+        break;
+      case '4':
+        viewHistory();
+        mainMenu();
+        break;
+      case '5':
+        flushWallet();
+        break;
+      case '6':
+        exit();
+        break;
+      default:
+        console.log('Invalid option.');
+        mainMenu();
+        break;
+    }
+  });
+}
+
+mainMenu();
+
+rl.on('close', () => {
+  process.exit(0);
+});
