f335e77d6fe65575ac9436ce584354c61fbace79
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-12-10 13:46:02 +0300

    feat: adding cin entry actions

--- cin.js
+++ cin.js
@@ -1,3 +1,18 @@
-function add(wallet, cin, label = "") {
-    return {...wallet}
+function isFull(wallet) {
+    return wallet.cinEntry.maxCapacity === wallet.cinEntry.list.length
 }
+
+
+function add(wallet, cin) {
+    if (!isFull(wallet))
+        return {...wallet.cinEntry, list: [...wallet.cinEntry.list, cin]}
+    console.log("The cin entry is Full")
+}
+
+function remove(wallet, cin) {
+    let list = [...wallet.cinEntry.list];
+    return list.filter(currentCin => currentCin.id !== cin.id);
+}
+
+exports.remove = remove
+exports.add = add
